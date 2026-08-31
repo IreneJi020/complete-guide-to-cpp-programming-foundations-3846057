e013f25c89ebfde1354dd145353b68498b006184
IreneJi020 <ireneyiji020@gmail.com>
2026-08-31 20:29:36 +0000

    unsigned int

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,26 @@
 
 #include <iostream>
 
+int a, b = 5; // global variables (try to avoid)
+
 int main(){
-    
+    bool my_flag; // local variable
+    a = 7;
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "my_flag = " << my_flag << std::endl;
+
+    my_flag = true;
+    std::cout << "my_flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
+    // 2^32 - 2 in binary
+
     std::cout << std::endl << std::endl;
     return 0;
 }
