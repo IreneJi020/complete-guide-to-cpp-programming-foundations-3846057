41e1e344aeeed63ed984a5284d6d6b49edc733b6
IreneJi020 <ireneyiji020@gmail.com>
2026-08-31 19:33:08 +0000

    cstdio

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -8,6 +8,8 @@ int main(){
     std::cout << "Hello World!" << std::endl;
 
     std::cout << std::endl << std::endl;
-    
+
     return 0;
 }
+
+// or use #include <cstdio> and printf("Hello World!\n");
